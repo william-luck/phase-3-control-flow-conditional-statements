fa4c90429f29d2a62ec01fd225bd5880ab0daa26
William Luck <luck.william.h@gmail.com>
2022-07-25 14:37:00 -0500

    commit tree 80150e5d5b95c82fc2d893e2c58651fbf1734e76

--- control_flow.rb
+++ control_flow.rb
@@ -1,16 +1,46 @@
+require 'pry'
+
 def admin_login(username, password)
-  # your code here
+  if (username == 'admin' || username == "ADMIN") && (password == '12345')
+    "Access granted"
+  else
+    "Access denied"
+  end
 end
 
+
 def hows_the_weather(temperature)
-  # your code here
+  case temperature
+  when ..39 then "It's brisk out there!"
+  when 40..65 then "It's a little chilly out there!"
+  when 85.. then "It's too dang hot out there!"
+  else "It's perfect out there!"
+  end
 end
 
 def fizzbuzz(num)
-  # your code here
+  if num%3 == 0
+    if num%5 == 0
+      return "FizzBuzz"
+    end
+    "Fizz"
+  elsif num%5 == 0
+    "Buzz"
+  else
+    num
+  end
 end
 
 def calculator(operation, num1, num2)
-  # your code here
+  case operation
+  when '+' then num1 + num2
+  when '-' then num1 - num2
+  when '*' then num1 * num2
+  when '/' then num1/num2
+  else
+    puts 'Invalid operation!'
+  end
 end
+
+
 
